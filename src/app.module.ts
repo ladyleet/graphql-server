c0274a96efc532116005a774fc5414fa3c39ce47
Tracy Lee <tracy@modern-web.org>
2019-07-21 17:31:07 -0400

    setup connection to mongo with mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService
+      ): Promise<MongooseModuleOptions> => {
+        return {
+          uri: configService.dbhost,
+          useNewUrlParser: true
+        };
+      },
+      inject: [ConfigService]
+    }),
+    ConfigModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
